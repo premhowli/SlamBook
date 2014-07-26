eabebb3d0faf84cbf3e7762b3664aae4a2f4131b
premhowli <howli.prem@gmail.com>
2014-07-26 23:57:44 +0530

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,10 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+config.serve_static_assets = true
+config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+config.assets.compile = true
+
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
